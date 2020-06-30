9c626efcda003a80361295e38f07ca9d13cbb672
= <dimitri.bonnet18@gmail.com>
2020-06-30 15:33:25 +0200

    Front Ok + RandomHero

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 /* Pour faire fonctionner mon CSS avec webpack */
 import "./style.css"
 
-const reponse = fetch("https://jsonplaceholder.typicode.com/users");
+
+/* function RandomNumber(min, max) {
+    const randoomNumber =  Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(randoomNumber)
+  } */
+
+
+/* const id = 20; */
+/* const reponse = fetch("https://superheroapi.com/api/2541564006094962/" + id);
 
 reponse
     .then(async res =>{
         console.log(res);
         try{
-            const users = await res.json();
-            console.log(users);
+            const hero = await res.json();
+            console.log(hero);
         } catch(e){
             console.log(e)
         }
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log(err)); */
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,11 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"]
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader',],
+      },
     ]
   },
   plugins: [
